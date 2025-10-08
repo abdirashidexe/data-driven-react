c3b7c726dae94fb9bd0a3ae15889560c65cb4bac
Abdi Ahmed <89213120+abdirashidexe@users.noreply.github.com>
2025-10-08 08:06:05 -0700

    contactcard & joke follow along

--- App.jsx
+++ App.jsx
@@ -1,5 +1,7 @@
 import Header from "./components/Header"
 import Entry from "./components/Entry"
+import Contact from "./components/ContactCard"
+import Joke from "./components/Joke"
 
 export default function App() {
     return (
@@ -7,6 +9,41 @@ export default function App() {
             <Header />
             <main className="container">
                 <Entry />
+                {/* <div className="contacts">
+                    <Contact
+                        img="./images/mr-whiskerson.png"
+                        name="Mr. Whiskerson"
+                        phone="(212) 555-1234"
+                        email="mr.whiskaz@catnap.meow"
+                    />
+                    <Contact
+                        img="./images/fluffykins.png"
+                        name="Fluffykins"
+                        phone="(212) 555-2345"
+                        email="fluff@me.com"
+                    />
+                    <Contact
+                        img="./images/felix.png"
+                        name="Felix"
+                        phone="(212) 555-4567"
+                        email="thecat@hotmail.com"
+                    />
+                    <Contact
+                        img="./images/pumpkin.png"
+                        name="Pumpkin"
+                        phone="(0800) CAT KING"
+                        email="pumpkin@scrimba.com"
+                    />
+                </div> */}
+                    <Joke
+                        setup="why did the chicken cross the road?"
+                        punchline="other side!"
+                    />
+                   <Joke
+                        setup="why joke 2 lol"
+                        punchline="xD side!"
+                    />
+                    
             </main>
         </>
     )
---
+++ components/Joke.jsx
@@ -0,0 +1,8 @@
+export default function Joke(props) {
+    return(
+        <main>
+            <h1>{props.setup}</h1>
+            <h2>{props.punchline}</h2>
+        </main>
+    )
+}
